934647dd12c4cbec2a0dc6cb9551544c74c0d02f
daikiando <daikiando@andoutaikinoMacBook-Pro.local>
2023-11-06 11:19:41 +0900

    www

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"os"
+	"strconv"
+)
+
 // AppConfig はアプリケーションの設定情報を管理
 type AppConfig struct {
 	DatabaseURL string
@@ -8,9 +13,28 @@ type AppConfig struct {
 
 // LoadConfig は設定情報を読み込み
 func LoadConfig() *AppConfig {
-	// 実際の設定情報をここで読み込む
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		// 環境変数から設定を読み込めない場合のデフォルト値
+		databaseURL = "mysql://root:パスワード@/financial_app"
+	}
+
+	apiPortStr := os.Getenv("API_PORT")
+	var apiPort int
+	if apiPortStr != "" {
+		var err error
+		apiPort, err = strconv.Atoi(apiPortStr)
+		if err != nil {
+			// パースエラーが発生した場合のデフォルト値
+			apiPort = 8080
+		}
+	} else {
+		// 環境変数が設定されていない場合のデフォルト値
+		apiPort = 8080
+	}
+
 	return &AppConfig{
-		DatabaseURL: "mysql://root:新しいパスワード@/financial_app",
-		APIPort:     8080,
+		DatabaseURL: databaseURL,
+		APIPort:     apiPort,
 	}
 }
